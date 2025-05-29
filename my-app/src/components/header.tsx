18ed464e0ef44c280c482d2ee3fa3d8a7559ca3d
mona <rostami.m4813@gmail.com>
2025-05-29 07:22:40 -0700

    complete navbar layout

--- my-app/src/components/header.tsx
+++ my-app/src/components/header.tsx
@@ -1,6 +1,68 @@
-function Header(){
-    return(
-        <></>
-    )
+import { useState } from "react";
+import { CgFormatJustify } from "react-icons/cg";
+import { FaFacebook } from "react-icons/fa6";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaTelegram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+
+function Header() {
+  const [Nv, Setnave] = useState(false);
+  const menue = () => {
+    Setnave(!Nv);
+  };
+  return (
+    <div className="  w-[100%] h-[100vh]  ">
+      <div
+        onClick={menue}
+        className=" md:hidden absolute top-[15px] left-[93vw]"
+      >
+        <CgFormatJustify size={"19px"} />
+      </div>
+      <ul className=" max-md:hidden px-4 flex justify-end  pl-3">
+        <li className="p-3 z-10">gallery</li>
+        <li className="p-3 z-10">gallery</li>
+        <li className="p-3 z-10">gallery</li>
+        <li className="p-3 z-10">gallery</li>
+      </ul>
+      <div className=" absolute flex w-[100%] bg-cover bg-amber-600 max-md:justify-end max-md:mt-12 -translate-y-12  ">
+        <div className="p-3">
+          {" "}
+          <FaFacebook />
+        </div>
+        <div className="p-3">
+          {" "}
+          <IoLogoInstagram />
+        </div>
+        <div className="p-3">
+          <FaTelegram />
+        </div>
+        <div className="p-3">
+          <FaLinkedin />
+        </div>
+      </div>
+      {/* mobile */}
+
+      <div
+        onClick={menue}
+        className={Nv ? "absolute  w-[100%] h-[100vh]   " : "hidden"}
+      >
+        <ul className=" text-[20px]  h-[100vh] w-[100%]  bg-black/85 flex  flex-col gap-y-[80px]  justify-center items-center text-amber-50">
+          <li>
+            <a href="#gallery">gallery</a>
+          </li>
+
+          <li>
+            <a href="#gallery">gallery</a>
+          </li>
+          <li>
+            <a href="#gallery">gallery</a>
+          </li>
+          <li>
+            <a href="#gallery">gallery</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
--- my-app/src/components/navbar.tsx
+++ my-app/src/components/navbar.tsx
@@ -1,6 +1,31 @@
-function Navbar(){
-    return(
-        <></>
-    )
+import { CgFormatJustify } from "react-icons/cg";
+import { MdPhoneEnabled } from "react-icons/md";
+import { BsFillPaletteFill } from "react-icons/bs";
+import "react";
+import { LuClock2 } from "react-icons/lu";
+
+function Navbar() {
+  return (
+    <div className="flex justify-between  p-4 w-[100%]  md:h-16 bg-amber-300 bg-cover">
+      <div>
+        <BsFillPaletteFill />
+      </div>
+
+      <div className="  flex ">
+        <div className=" translate-x-[-25px] translate-y-[-2px]">
+          <p className=" ">9am </p>
+        </div>
+        <div className=" translate-y-[2px] translate-x-[-18px]">
+          <LuClock2 />
+        </div>
+
+        <p className=" translate-x-[-5px]   translate-y-[-2px]">123-456-789</p>
+        <div className="pr-4 translate-y-[2px]">
+          <MdPhoneEnabled />
+        </div>
+       
+      </div>
+    </div>
+  );
 }
-export default Navbar
+export default Navbar;
