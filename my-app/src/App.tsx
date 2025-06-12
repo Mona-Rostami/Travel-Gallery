4b9c47d8713b9d65b0245d220917363fc1f967e6
mona <rostami.m4813@gmail.com>
2025-06-12 13:00:06 -0700

    import component in App

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar";
 import Header from "./components/header";
 import Photo from "./components/photo";
 import Massage from "./components/massege";
+import Album from "./components/album";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Photo />
       <Gallery />
+      <Album/>
       <Massage />
     </div>
   );
