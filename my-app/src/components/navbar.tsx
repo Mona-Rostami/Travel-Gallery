831baa6d0dedafdc53e6240e54182720f18630c5
mona <rostami.m4813@gmail.com>
2025-06-12 12:48:35 -0700

    Add navbar component

--- my-app/src/components/navbar.tsx
+++ my-app/src/components/navbar.tsx
@@ -1,27 +1,28 @@
-import { CgFormatJustify } from "react-icons/cg";
+
 import { MdPhoneEnabled } from "react-icons/md";
-import { BsFillPaletteFill } from "react-icons/bs";
+import { BsChatSquareDots } from "react-icons/bs";
 import "react";
 import { LuClock2 } from "react-icons/lu";
 
 function Navbar() {
   return (
-    <div className="flex justify-between  p-4 w-[100%]  md:h-16 bg-amber-300 bg-cover">
-      <div>
-        <BsFillPaletteFill />
+    <div className="flex justify-between lg:p-16 w-full h-[8vh]  bg-gray-700 bg-cover">
+      <div className="text-[5vw] translate-y-[2vh] pl-2">
+        <BsChatSquareDots  />
       </div>
+               {/* <button  className=" translate-x-[-10%] h-10 "> Get a Free Quote</button> */}
 
-      <div className="  flex ">
-        <div className=" translate-x-[-25px] translate-y-[-2px]">
-          <p className=" ">9am </p>
+      <div className=" max-md:hidden flex ">
+        <div className="md:text-[3vw] mr-[1vw] translate-y-[-5%] ">
+          <p className="">9am </p>
         </div>
-        <div className=" translate-y-[2px] translate-x-[-18px]">
+        <div className=" md:text-[3vw] mr-[2vw]  translate-y-[10%]">
           <LuClock2 />
         </div>
 
-        <p className=" translate-x-[-5px]   translate-y-[-2px]">123-456-789</p>
-        <div className="pr-4 translate-y-[2px]">
-          <MdPhoneEnabled />
+        <p className="md:text-[3vw] translate-x-[-3%] translate-y-[-2%]">123-456-789</p>
+        <div className=" md:text-[3vw]  mr-[3vw] translate-y-[5%]  ">
+          <MdPhoneEnabled  />
         </div>
        
       </div>
