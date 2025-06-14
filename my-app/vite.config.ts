7351f7b7e8550c7d9623cd6d33886804ef17d6a1
mona <rostami.m4813@gmail.com>
2025-06-14 06:26:46 -0700

    Add file

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/Travel-Gallery/',
-  plugins: [react(),    tailwindcss(),
+  base:'/Travel-Gallery/', 
+  plugins: [react(),    tailwindcss()
 ],
 })
