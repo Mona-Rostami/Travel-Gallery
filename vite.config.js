babc5bf8eb40a3918746efa836661d8dbb9efbe1
mona <rostami.m4813@gmail.com>
2025-06-26 11:25:39 -0700

    add commit three

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { plugin } from 'postcss'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    [react()],
   ],
+      base:'/Travel-Gallery/'
+
 })
